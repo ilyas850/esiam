4ebaa0df262ef825f8f3e1d81112ac4631eabcd5
ilyasr974 <ilyasr974@gmail.com>
2022-06-20 14:09:06 +0700

    tabel soal

--- resources/views/sadmin/soal/soal_ujian.blade.php
+++ resources/views/sadmin/soal/soal_ujian.blade.php
@@ -11,7 +11,7 @@
                 <h3 class="box-title">Data Soal UTS dan UAS</h3>
             </div>
             <div class="box-body">
-                <table id="example4" class="table table-bordered table-striped">
+                <table id="example1" class="table table-bordered table-striped">
                     <thead>
                         <tr>
                             <th>
