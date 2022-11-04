118fcbd53adfd4e3d9a99aae0a15189ba5f71193
ilyasr974 <ilyasr974@gmail.com>
2022-11-04 10:45:47 +0700

    konversi keuangan

---
+++ app/Konversi_itembayar.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Konversi_itembayar extends Model
+{
+    protected $table = 'konversi_itembayar';
+
+    protected $primaryKey = 'id_konversi_itembayar';
+}
