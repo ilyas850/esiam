6f88aa5e42e9302269b28a375f6b8af50f21b74c
ilyasr974 <ilyasr974@gmail.com>
2023-06-12 09:10:32 +0700

    cetak soal

--- resources/views/dosenluar/makul_diampu.blade.php
+++ resources/views/dosenluar/makul_diampu.blade.php
@@ -284,6 +284,15 @@
                                                     <input type="file" class="form-control" name="soal_uas">
                                                     <span>Max. size 4 mb dengan format (.pdf) atau (.doc)</span>
                                                 </div>
+                                                <div class="form-group">
+                                                    <label>Cetak Soal</label>
+                                                    <select name="cetak_soal_uas" class="form-control" required>
+                                                        <option value="{{ $item->cetak_soal_uas }}">
+                                                            {{ $item->cetak_soal_uas }}</option>
+                                                        <option value="YA">YA</option>
+                                                        <option value="TIDAK">TIDAK</option>
+                                                    </select>
+                                                </div>
                                                 <button type="submit" class="btn btn-primary">Simpan</button>
                                             </form>
                                         </div>
